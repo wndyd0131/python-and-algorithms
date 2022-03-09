066fe74006b6f028dfff6d5f31259c277a416559
wndyd <wndyd0131@naver.com>
2022-03-10 00:10:54 +0900

    11286

--- baekjoon/11286.py
+++ baekjoon/11286.py
@@ -1,15 +1,17 @@
-# 절댓값이 같은 경우 실제값이 작은 애부터
-# 절댓값이 작은 애
 import sys, heapq
 heap = []
 value = 0
 N = int(sys.stdin.readline())
 for i in range(N):
-  input = -(int(sys.stdin.readline()))
+  input = int(sys.stdin.readline())
   if input == 0:
     if heap:
-      print(heapq.heappop(heap))
+      print(heapq.heappop(heap)[1])
     else:
       print(0)
   else:
-    heapq.heappush(heap, input)
+    heapq.heappush(heap, (abs(input), input))
+    
+
+# heapq는 튜플도 가능하다
+# 튜플 (abs(값), 값) 을 사용해서 절댓값이랑 비교하되 절댓값이 같은 값끼리는 원래 값과 비교하도록 할 수 있음
