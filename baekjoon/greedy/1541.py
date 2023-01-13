edee7cb71e20b4d905445139d04fa40ea47c817a
wndyd <wndyd@JUYONGui-MacBookAir.local>
2023-01-14 06:03:21 +0900

    1541

---
+++ baekjoon/greedy/1541.py
@@ -0,0 +1,26 @@
+import sys
+equation = sys.stdin.readline().strip()
+temp = '0'
+sum = 0
+lst = []
+for i in range(len(equation)):
+    if equation[i].isdigit():
+        temp += equation[i]
+        if i == len(equation)-1:
+            sum += int(temp)
+            lst.append(sum)
+            temp = ''
+    else:
+        if equation[i] == '-':
+            sum += int(temp)
+            lst.append(sum)
+            temp = ''
+            sum = 0
+        else:
+            sum += int(temp)
+            temp = ''
+
+num = lst[0]
+for i in range(1, len(lst)):
+    num -= lst[i]
+print(num)
