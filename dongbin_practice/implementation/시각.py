3fec97dcdaff39c77f8e58affbb7409607cde41e
wndyd <wndyd0131@naver.com>
2023-01-27 11:41:42 +0900

    나동빈: 시각

---
+++ dongbin_practice/implementation/시각.py
@@ -0,0 +1,10 @@
+import sys
+N = int(sys.stdin.readline())
+count = 0
+
+for i in range(N+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                count +=1
+print(count)
