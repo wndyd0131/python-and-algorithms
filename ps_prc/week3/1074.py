e2d1cd6cf2ea6704101abbe1f97ece6daf4adc2d
wndyd <wndyd0131@naver.com>
2023-03-13 02:26:37 +0900

    1074 done

--- ps_prc/week3/1074.py
+++ ps_prc/week3/1074.py
@@ -1,33 +1,34 @@
 import sys
 sys.setrecursionlimit(10 ** 6)
-# 3 (0,0) (0,4) (4,0) (4,4)
-# 2 (0,0) (0,2) (2,0) (2,2)
-# 1 (0,0) (0,1) (1,0) (1,1)
-# 0
 
 def func(x, y, n):
   global cnt
-  global passed
-  if passed == True:
+  if x > r or x+n <= r or y > c or y+n <= c:
+    cnt += n**2
     return
-  if n == 0:
-    if x == r and y == c:
-      passed = True
-    cnt += 1
-    return
-  num = 2**n // 2
-  func(x,y,n-1)
-  func(x,y+num,n-1)
-  func(x+num,y,n-1)
-  func(x+num,y+num,n-1)
-    
+  
+  if n > 2:
+    n //= 2  
+    func(x,y,n)
+    func(x,y+n,n)
+    func(x+n,y,n)
+    func(x+n,y+n,n)
+  else:
+    if x==r and y==c:
+      print(cnt)
+    elif x==r and y+1==c:
+      print(cnt+1)
+    elif x+1==r and y==c:
+      print(cnt+2)
+    else:
+      print(cnt+3)
+    sys.exit()
 
 N, r, c = list(map(int, sys.stdin.readline().split()))
-cnt = -1
-passed = False
+cnt = 0
 # arr = [[0] * 2**N for _ in range(2**N)]
 if r == 0 and c == 0:
   print(0)
 else:
-  func(0,0,N)
+  func(0,0,2**N)
   print(cnt)
