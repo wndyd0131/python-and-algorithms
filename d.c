7056873d5cd31edeccad4091f7bfd98f41a7fd3d
wndyd <wndyd0131@naver.com>
2023-03-13 22:55:37 +0900

    5904 trial2

---
+++ d.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+  int arr[30];
+  printf("%d", sizeof(int));
+}
--- ps_prc/week3/5904.py
+++ ps_prc/week3/5904.py
@@ -1,12 +1,29 @@
 import sys
 
-def repeat(string, cnt):
-  if len(string) >= N:
-    # print(string)
-    print(string[N-1])
-    return
-  n_string = string + "m" + "o" * (cnt + 2) + string
-  repeat(n_string, cnt+1)
-
 N = int(sys.stdin.readline())
-repeat("moo", 1)
+string = "moo"
+i = 1
+while len(string) < N:
+  orig_string = string
+  mid_string = "m" + "o"*(i+2)
+  string = orig_string + mid_string + orig_string
+  i += 1
+
+print(string)
+
+if N >= 4:
+  if N <= len(orig_string):
+    print(orig_string[N-1])
+  elif N > len(orig_string) and N <= len(orig_string + mid_string):
+    if N > len(orig_string)+1:
+      print("o")
+    else:
+      print("m")
+  else:
+    N = N - len(orig_string+mid_string)
+    print(orig_string[N-1])
+else:
+  if N == 0:
+    print("m")
+  else:
+    print("o")
