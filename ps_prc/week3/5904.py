11fdebfc2a59fc8f93a15781b6384d8b0d963a17
wndyd <wndyd0131@naver.com>
2023-03-13 23:24:26 +0900

    5904 final

--- ps_prc/week3/5904.py
+++ ps_prc/week3/5904.py
@@ -1,29 +1,24 @@
 import sys
-
-N = int(sys.stdin.readline())
+sys.setrecursionlimit(10 ** 6)
 string = "moo"
-i = 1
-while len(string) < N:
-  orig_string = string
-  mid_string = "m" + "o"*(i+2)
-  string = orig_string + mid_string + orig_string
-  i += 1
+N = int(sys.stdin.readline())
 
-print(string)
+def bt(n, depth, b_len):
+  new_length = 2*b_len + depth + 3
+  
+  if n < 4:
+    return
 
-if N >= 4:
-  if N <= len(orig_string):
-    print(orig_string[N-1])
-  elif N > len(orig_string) and N <= len(orig_string + mid_string):
-    if N > len(orig_string)+1:
-      print("o")
+  if new_length < n:
+    bt(n, depth+1, new_length)
+  else:
+    if n > b_len and n <= b_len + depth + 3:
+      if n - b_len == 1:
+        print("m")
+      else:
+        print("o")
+      return
     else:
-      print("m")
-  else:
-    N = N - len(orig_string+mid_string)
-    print(orig_string[N-1])
-else:
-  if N == 0:
-    print("m")
-  else:
-    print("o")
+      bt(n-(b_len+depth+3),1,3)
+
+bt(N,1,3)
