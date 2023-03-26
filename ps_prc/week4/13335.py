df322a098498ae3a357f4d599e4c2efcadfac48c
wndyd <wndyd0131@naver.com>
2023-03-26 19:48:06 +0900

    13335 trial2

--- ps_prc/week4/13335.py
+++ ps_prc/week4/13335.py
@@ -1,27 +1,16 @@
 import sys
-from collections import deque
-queue = deque([])
-answer = 0
-
+time = 0
 def solution(w, L):
-    global answer
-    total = 0
-    for i in range(len(arr)):
-        if len(queue) == w: #큐가 꽉찼다면 비우기
-            popped = queue.popleft()
-            total -= popped
-        while total + arr[i] > L:
-            popped = queue.popleft()
-            total -= popped
-        if len(queue) == 0:
-            queue.append(arr[i])
-            total += arr[i]
-            answer += w
-        else:
-            queue.append(arr[i])
-            total += arr[i]
-            answer += 1
-    answer += 1
+    global time
+    bridge = [0] * w
+    while bridge:
+        time += 1
+        bridge.pop(0)
+        if arr:
+            if sum(bridge) + arr[0] <= L:
+                bridge.append(arr.pop(0))
+            else:
+                bridge.append(0)
 
 cmd = list(map(int, sys.stdin.readline().split()))
 w = int(cmd[1])
@@ -29,4 +18,4 @@ L = int(cmd[2])
 arr = list(map(int, sys.stdin.readline().split()))
 
 solution(w, L)
-print(answer)
+print(time)
